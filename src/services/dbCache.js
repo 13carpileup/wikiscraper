53224167b5ea6aa7ea4989e275406a920e317902
Alex <climiealex@gmail.com>
2024-12-19 14:02:34 +0800

    postgres cache

---
+++ src/services/dbCache.js
@@ -0,0 +1,102 @@
+const FileService = require('./fileService');
+const { Pool } = require('pg');
+
+
+class DBCache {
+    static pool = new Pool({
+        user: 'your_username',
+        host: 'localhost',
+        database: 'pathfinder_db',
+        password: 'your_password',
+        port: 5432,
+    });
+
+    static cache = new Map();
+
+    static async initDB() {
+        // console.log("dropping...")
+        // const query = await DBCache.pool.query(`DROP TABLE IF EXISTS path_cache;`)
+        const result = await DBCache.pool.query(`
+            CREATE TABLE IF NOT EXISTS path_cache (
+                id SERIAL PRIMARY KEY,
+                initial TEXT,
+                target TEXT,
+                paths TEXT,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            )
+        `);
+    }
+
+    static async checkCache(initial, target) {
+        let query = {
+            text: 'SELECT paths FROM path_cache WHERE initial = $1 AND target = $2',
+            values: [initial, target],
+        };
+
+        let result = await DBCache.pool.query(query);
+        if (result.rows.length === 0) {
+            return false;
+        }
+        console.log("PATHS:")
+        console.log(result.rows[0].paths);
+        let paths = JSON.parse(result.rows[0].paths);
+
+        if (paths) {
+            if (paths.length > 0) {
+                return paths;
+            }
+
+            return false;
+        }
+
+        return false;
+    }
+
+    static async addCache(initial, target, path) {
+        for (let i = 0; i < path.length - 1; i++) {
+            for (let j = i + 1; j < path.length; j++) {
+
+                let spliceArray = path.slice(i, j + 1);
+
+                let newFrom = spliceArray[0];
+                let newTo = spliceArray[spliceArray.length - 1];
+
+                let query = {
+                    text: 'SELECT paths FROM path_cache WHERE initial = $1 AND target = $2',
+                    values: [newFrom, newTo],
+                };
+
+                let result = await DBCache.pool.query(query);
+
+                if (result.rows.length === 0) {
+                    //console.log(`creating... ${newFrom} to ${newTo}`)
+                    query = {
+                        text: 'INSERT INTO path_cache (initial, target, paths) VALUES ($1, $2, $3)',
+                        values: [newFrom, newTo, JSON.stringify([spliceArray])],
+                    };
+
+                    result = await DBCache.pool.query(query);
+                    //console.log(result);
+                    continue;
+                }
+
+                let paths = JSON.parse(result.rows[0].paths);
+
+                if (paths.includes(spliceArray)) continue;
+
+                paths.push(spliceArray);
+                query = {
+                    text: 'UPDATE path_cache SET paths = $1 WHERE initial = $2 AND target = $3;',
+                    values: [JSON.stringify(paths), newFrom, newTo],
+                };
+
+                result = await DBCache.pool.query(query);
+            }
+        }
+
+        return false;
+    }
+
+}
+
+module.exports = DBCache;
--- src/services/pathFinderService.js
+++ src/services/pathFinderService.js
@@ -1,57 +1,16 @@
 const FileService = require('./fileService');
+const DBCache = require('./dbCache');
+
 
 class PathFinderService {
-    static cache = new Map();
-
-    static checkCache(initial, target) {
-        const key = `${initial}-${target}`;
-        if (PathFinderService.cache.has(key)) {
-            let res = PathFinderService.cache.get(key);
-
-            if (res.length > 0) {
-                return res;
-            }
-
-            return false;
-        }
-
-        PathFinderService.cache.set(key, []);
-        return false;
-    }
-
-    static addCache(initial, target, path) {
-        for (let i = 0; i < path.length - 1; i++) {
-            for (let j = i + 1; j < path.length; j++) {
-
-                let spliceArray = path.slice(i, j + 1);
-
-                let newFrom = spliceArray[0];
-                let newTo = spliceArray[spliceArray.length - 1];
-
-                let key = `${newFrom}-${newTo}`;
-                console.log(key)
-
-                let res = PathFinderService.checkCache(newFrom, newTo);;
-                if (!res) {
-                    PathFinderService.cache.set(key, [spliceArray]);
-                    continue;
-                }
-
-                if (res.includes(spliceArray)) continue;
-
-                res.push(spliceArray);
-                PathFinderService.cache.set(key, res);
-            }
-        }
-
-        return false;
-    }
-
     static async findPaths(initial, target) {
-        await FileService.logRequest(initial, target);
-        let res = PathFinderService.checkCache(initial, target);
+        await DBCache.initDB();
 
-        if (res) return res;
+        await FileService.logRequest(initial, target);
+        let res = await DBCache.checkCache(initial, target);
+        console.log(res);
+
+        if (res && res.length!=0) return res;
 
         const visited = new Map();
         const queue = [[[initial], await FileService.readFile(initial)]];
@@ -76,7 +35,7 @@ class PathFinderService {
 
                 if (newConnections.includes(target)) {
                     foundPaths.push(currentPath.concat([con, target]));
-                    PathFinderService.addCache(initial, target, currentPath.concat([con, target]));
+                    await DBCache.addCache(initial, target, currentPath.concat([con, target]));
                     foundLength = currentPath.length;
                 }
             }
