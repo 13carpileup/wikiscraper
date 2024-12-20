24e404a54963d339983c8dc7105dba774f977569
Alex <climiealex@gmail.com>
2024-12-20 12:13:35 +0800

    data cleansing

--- dataGen/clean.py
+++ dataGen/clean.py
@@ -1,17 +1,16 @@
 import os
 
-allFiles = [f for f in os.listdir('data/')]
+allFiles = [f for f in os.listdir('./data/')]
 c = 0
 for file in allFiles:
     f = open(f'data/{file}', 'r')
     cons = f.read().split('\n')
 
-    if (len(cons) < 2 and cons[0] == file[:-4]):
-        c += 1
+
+    if (len(cons)==0 or not cons[0]):
         print(file)
 
-print(c)
 
-# Visual J++.txt 
+# Visual J++.txt
 # GamesRadar+
 # Redirect to themselves for some reason? But they do have real wikipedia files???
